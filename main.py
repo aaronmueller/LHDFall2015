7340fb743a81585584a7365282d16a1380693416
Aaron Mueller <ammaaronmueller@gmail.com>
2015-10-10 15:55:33 -0400

    Concatenated simpleGraphics and main

--- main.py
+++ main.py
@@ -73,11 +73,32 @@ def main():
 
         yes = Text(Point(100,100), "YAY")
         yes.draw(winGame)
-        
-    
-
 
     winMenu.getMouse()
     winMenu.close()
 
 main()
+
+def game():
+
+    win = GraphWin("Game", 500, 500)
+    win.setBackground("#EFDECD")
+
+    bike1 = Rectangle(Point(1,1),Point(15,15))
+    bike1.setFill("#CCCCFF")
+    bike1.draw(win)
+
+    for i in range(3):
+   
+        win.getMouse()
+        bike1.move(15,0)
+
+        win.getMouse()
+
+        bike1.move(0,15)
+
+    win.getMouse()
+
+    bike1.undraw()
+
+    win.close()
