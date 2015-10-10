66620a028e14e2cb2bd7f137e5c7313525eee19f
Aaron Mueller <ammaaronmueller@gmail.com>
2015-10-10 15:19:11 -0400

    Added Snake.py; deleted main.cpp

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # LHDFall2015
 # Hello, It's Jacob.
 
+# This is a game made in Python.
+# It is a multiplater version of snake for 2-4 players. Online integration has not yet been added.
---
+++ Snake.py
@@ -0,0 +1,16 @@
+class Snake:
+    
+    def _init_(self, color):
+        self.color = color
+        length = 5
+    
+    def getLength():
+        return length
+
+    def getColor():
+        return color
+
+    def incLength():
+        length++
+    
+
